9f757096136833779b15bb2ba195b9cbb703d12f
Karol Morawski <karol.morawski4@gmail.com>
2019-10-24 11:28:19 +0200

    Revert "ecks dee"
    This reverts commit 7dd9d828cb2d6f4efe2a0165a394756f80ec0fec.

--- Hist1/Main.py
+++ Hist1/Main.py
@@ -1,6 +1,6 @@
 import pgraphdraw as pg
 import pdepgen as pd
 
-dependency_array = pd.get_graph("../test/")
+dependency_array = pd.get_graph()
 print(dependency_array)
 pg.drawGraph(dependency_array)
