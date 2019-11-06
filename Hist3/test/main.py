c4e1b621a8056c1dff72846c59343bb4034e7739
Karol Morawski <karol.morawski4@gmail.com>
2019-11-06 23:16:22 +0100

    Added Hist3 (not finished)

---
+++ Hist3/test/main.py
@@ -0,0 +1,11 @@
+import pgraphdraw as pg
+import graphgen as pd
+import sys
+
+directory = "./"
+if len(sys.argv)>1:
+    directory = sys.argv[1]
+
+dependency_array = pd.get_graph(directory)
+print(dependency_array)
+pg.drawGraph(dependency_array)
