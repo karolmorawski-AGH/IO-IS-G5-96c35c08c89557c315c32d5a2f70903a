cb482c13afb0fd07b8c08197d172a2a1e8f7d81e
Aleksandra Nowotny <48154457+anowotny@users.noreply.github.com>
2019-11-06 12:29:23 +0100

    test functionsfor hist3

---
+++ Hist1/test2.py
@@ -0,0 +1,14 @@
+import test1
+import test3
+
+def druga():
+    return 2*2*2
+def dwa_i_pol():
+    napis="skhdgkjdbc"
+    print("Jestem funkcja z test2 a to napis "+napis)
+def abc():
+    return 10000
+
+test1.pierwsza()
+test3.trzecia()
+test3.trzy_i_pol()
