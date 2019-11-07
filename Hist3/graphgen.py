898fca665850bb479197d6ebf69564e315371f17
Karol Morawski <karol.morawski4@gmail.com>
2019-11-07 17:29:02 +0100

    Added generation of directional graph representation

--- Hist3/graphgen.py
+++ Hist3/graphgen.py
@@ -79,25 +79,6 @@ def get_func_list(filepath):
     
     return func_array
 
-
-#Getting func/methods names CALLED
-import dis
-def list_func_calls(fn, file):
-    
-    import pgraphdraw
-
-    #fn = eval(fn)
-    funcs = []
-    bytecode = dis.Bytecode(fn)
-    instrs = list(reversed([instr for instr in bytecode]))
-    for (ix, instr) in enumerate(instrs):
-        if instr.opname=="CALL_FUNCTION":
-            load_func_instr = instrs[ix + instr.arg + 1]
-            #if load_func_instr.opname == "LOAD_GLOBAL":
-            funcs.append(load_func_instr.argval)
-
-    return funcs
-
 def get_graph_func(directory):
     files = []
     declared_f = []
@@ -107,32 +88,127 @@ def get_graph_func(directory):
         if filter_non_py(file) == 1:
             files.append(file[:-3])
     
-    #Declared funcs
     for file in os.listdir(directory):
         declared_f.append(get_func_list(file))
-        called_f.append(get_func_calls_list(file))
-
+        called_f.append(list_func_calls(file))
     #Called funcs                   
-
-    #print("Declared:\n\n")
-    #print(declared_f)
-    #print("\n\nCalled:\n")
-    #print(called_f)
-    return 0
-
-#THIS FUNCTION ALWAYS AS LAST FUNCTION IN FILE
-def get_func_calls_list(file):
-    f_number = len(get_func_list(file))
-    tab = [file]
-    i = 1 
-    while i < f_number:
-        print(get_func_list(file)[i])
-        tab=tab+(list_func_calls(get_func_list(file)[i], file[:-3]))
+    '''
+    print("Declared:\n\n")
+    i = 0
+    while i < len(declared_f):
+        print(declared_f[i])
         i = i + 1
-    #print(tab)
-    return tab
+    print("\n\nCalled:\n")
+    i = 0
+    while i < len(called_f):
+        print(called_f[i])
+        i = i + 1
+    '''
+
+    w = len(called_f) + 1
+    h = len(declared_f) + 1
+    Matrix = [["" for x in range(w)] for y in range(h)] 
 
 
+    module_array = []
+    i = 0
+    while i < len(declared_f):
+        module_array.append(declared_f[i][0])
+        i = i + 1
+
+
+    i = 1
+    while i <  len(module_array)+1:
+        Matrix[0][i] = module_array[i-1]
+        i = i + 1
+
+    i = 1
+    while i <  len(module_array)+1:
+        Matrix[i][0] = module_array[i-1]
+        i = i + 1
+            
+    i = 1
+    while i < len(Matrix[0]):
+        j = 1
+        while j < len(Matrix):
+            if i == j:
+                Matrix[i][j] = "0"
+            else:
+                #calling, declared
+                #print(i,j, Matrix[i][0], Matrix[j][0])
+                Matrix[i][j] = get_number_of_calls(Matrix[i][0], Matrix[j][0], called_f, declared_f)
+            j = j+1
+        i=i+1
+
+
+    print(Matrix)
+
+
+def get_number_of_calls(calling, declared, called_f, declared_f):
+    
+    #find calling array
+    #print(called_f)
+    call_desired = []
+    i = 0
+    while i < len(called_f):
+        if called_f[i][0] == calling:
+            call_desired = called_f[i]
+        i = i + 1
+
+    declared_desired = []
+    i = 0
+    while i < len(declared_f):
+        if declared_f[i][0] == declared:
+            declared_desired = declared_f[i]
+        i = i + 1
+    
+    #TODO count how much from call_desired names matches declared desired
+
+    return "-1"
+
+
+from collections import deque
+
+class FuncCallVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self._name = deque()
+
+    @property
+    def name(self):
+        return '.'.join(self._name)
+
+    @name.deleter
+    def name(self):
+        self._name.clear()
+
+    def visit_Name(self, node):
+        self._name.appendleft(node.id)
+
+    def visit_Attribute(self, node):
+        try:
+            self._name.appendleft(node.attr)
+            self._name.appendleft(node.value.id)
+        except AttributeError:
+            self.generic_visit(node)
+
+
+#Getting func/methods names CALLED
+def list_func_calls(filename):
+
+    tree = ast.parse(open(filename).read())
+
+    func_calls = [filename[:-3]]
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Call):
+            callvisitor = FuncCallVisitor()
+            callvisitor.visit(node.func)
+            func_calls.append(callvisitor.name)
+
+    return func_calls
+
+
+#print(list_func_calls("graphgen.py"))
 #list_func_calls("drawGraph", "pgraphdraw.py")
 #print(inspect.stack()[1].function)
 #pythonw -m trace -l graphgen.py 
+get_graph_func("./")
