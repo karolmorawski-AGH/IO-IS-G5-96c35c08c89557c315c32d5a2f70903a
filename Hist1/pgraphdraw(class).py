c3e89705a090473a0177c7f5daab0d5add5326ec
porlick7 <48184753+porlick7@users.noreply.github.com>
2019-11-13 20:36:36 +0100

    Add files via upload

---
+++ Hist1/pgraphdraw(class).py
@@ -0,0 +1,15 @@
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+class DrawGraph:
+    G = nx.DiGraph()
+    pos = self.nx.spring_layout(G)
+    def drawGraph(x):   
+        for i in range(0, len(x)):
+           G.add_edge(x[i][0], x[i][1], length=x[i][2])
+        nx.draw(G,pos,edge_color='black',width=1, node_size=1000,node_color='lightgreen', with_labels=True)
+        edge_labels=dict([((u,v,),d['length']) for u,v,d in G.edges(data=True)])
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.2)
+        plt.show()
+
