480358a9505984bef8ed0814f909c4f53643cb44
porlick7 <48184753+porlick7@users.noreply.github.com>
2019-11-14 10:52:41 +0100

    Add files via upload

--- Hist3/pgraphdraw(class).py
+++ Hist3/pgraphdraw(class).py
@@ -5,8 +5,7 @@ class DrawGraph:
    G = nx.DiGraph()
    pos = nx.planar_layout(G)
    color_map=[]
-   sum=[0]
-   def drawGraph(matrix):
+   def drawGraph(self,matrix):
 
     array = matrix[0]
     array2 = matrix[1]
@@ -28,7 +27,7 @@ class DrawGraph:
     for i in range(1, len(array)):
         for j in range(1, len(array)):
             if i != j and array[i][j] != "0":
-                G.add_edge(array[i][0] + "\n" + str(sum[i]), array[0][j] + "\n" + str(sum[j]), length=array[i][j])
+                self.G.add_edge(array[i][0] + "\n" + str(sum[i]), array[0][j] + "\n" + str(sum[j]), length=array[i][j])
 
     '''
     array2 = [[],
@@ -40,19 +39,17 @@ class DrawGraph:
     for i in range(1, len(array)):
         if len(array2[i-1]) > 0:
             for j in range (0, len(array2[i-1])):
-                G.add_edge(array2[i-1][j], array[i][0] + "\n" + str(sum[i]), length="")
+                self.G.add_edge(array2[i-1][j], array[i][0] + "\n" + str(sum[i]), length="")
 
    
-    for node in G:
+    for node in self.G:
         if ".py" not in node:
-            color_map.append('lightblue')
+            self.color_map.append('lightblue')
         else:
-            color_map.append('lightgreen')
+            self.color_map.append('lightgreen')
 
  
-    nx.draw(G,pos,edge_color='black',width=0.5, node_size=1000,node_color=color_map, with_labels=True, font_size = 10)
-    edge_labels=dict([((u,v,),d['length']) for u,v,d in G.edges(data=True)])
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, label_pos=0.4)
+    nx.draw(self.G,self.pos,edge_color='black',width=0.5, node_size=1000,node_color=self.color_map, with_labels=True, font_size = 10)
+    edge_labels=dict([((u,v,),d['length']) for u,v,d in self.G.edges(data=True)])
+    nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=edge_labels, label_pos=0.4)
     plt.show()
-
-
