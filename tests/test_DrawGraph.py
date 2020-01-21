1f068527dc8bb7155dc770f0e9386373d69c78c5
porlick7 <48184753+porlick7@users.noreply.github.com>
2020-01-21 20:17:45 +0100

    Add files via upload

--- tests/test_DrawGraph.py
+++ tests/test_DrawGraph.py
@@ -5,9 +5,8 @@ from src.ioproject import DrawGraph
 from src.ioproject import FileGraphGenerator
 import src.ioproject.graph_generator as gg
 import src.ioproject.graph_sketcher as gs
-
 class TestDrawGraph(unittest.TestCase):
-    directory = "./"
+    directory =gs.dirpath
     if len(sys.argv) > 1:
         directory = sys.argv[1]
 
@@ -22,21 +21,59 @@ class TestDrawGraph(unittest.TestCase):
 
     def test_draw_file_graph(self):
         self.assertEqual(gs.file_graph_gen, gg.FileGraphGenerator(self.directory))
-
+        self.assertTrue(gs.x)
+        self.assertTrue(gs.G)
+        self.assertTrue(gs.pos)
+        self.assertTrue(gs.edge_labels)
 
     def test_draw_method_graph(self):
+        self.assertEqual(gs.method_graph,gg.MethodGraphGenerator(self.directory))
         self.assertEqual(gs.module_graph_gen , gg.ModuleGraphGenerator(self.directory))
-
+        self.assertTrue(gs.graph)
+        self.assertTrue(gs.func)
+        self.assertTrue(gs.fileCode)
+        self.assertTrue(gs.complex)
+        self.assertTrue(gs.G)
+        self.assertTrue(gs.pos)
+        self.assertTrue(gs.edge_labels)
     def test_draw_module_graph(self):
+        self.assertEqual(gs.module_graph_gen,gg.ModuleGraphGenerator(self.directory))
+        self.assertTrue(gs.array)
+        self.assertTrue(gs.array2)
+        self.assertTrue(gs.graphx)
+        self.assertTrue(gs.sum>0)
+        self.assertFalse(gs.color_map.append(23435235))
+        self.assertFalse(gs.color_map.append('%#%!536'))
+        self.assertTrue(gs.pos)
+        self.assertTrue(gs.edge_labels)
         pass
 
     def test_draw_file_module_graph(self):
+        self.assertEqual(gs.module_graph_gen,gg.ModuleGraphGenerator(self.directory))
+        self.assertEqual(gs.file_graph_gen,gg.FileGraphGenerator(self.directory))
+        self.assertTrue(gs.x)
+        self.assertTrue(gs.array)
+        self.assertTrue(gs.array2)
+        self.assertTrue(gs.graphx)
+        self.assertTrue(gs.sum>0)
+        self.assertTrue(gs.file_array)
+        self.assertFalse(gs.file_sum<0)
+        self.assertTrue(gs.module_array)
         self.assertFalse(gs.color_map.append(23435235))
         self.assertFalse(gs.color_map.append('%#%!536'))
-
+        self.assertTrue(gs.pos)
+        self.assertTrue(gs.edge_labels)
 
     def test_draw_file_method_graph_direct(self):
+        self.assertEqual(gs.module_graph_gen,gg.ModuleGraphGenerator(self.directory))
+        self.assertTrue(gs.array)
+        self.assertTrue(gs.array2)
+        self.assertTrue(gs.graphx)
+        self.assertFalse(gs.color_map.append(23435235))
+        self.assertFalse(gs.color_map.append('%#%!536'))
+        self.assertTrue(gs.pos)
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
