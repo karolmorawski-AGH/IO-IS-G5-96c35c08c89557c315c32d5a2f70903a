2ba0b88120b95ac3bdd534b1a4244daca4339c81
porlick7 <48184753+porlick7@users.noreply.github.com>
2020-01-23 15:41:41 +0100

    Add files via upload

--- tests/test_MethodGraphGenerator.py
+++ tests/test_MethodGraphGenerator.py
@@ -1,22 +1,23 @@
 import unittest
 import sys
 sys.path.append('../')
-from src.ioproject import MethodGraphGenerator
+from src.ioproject.graph_generator import MethodGraphGenerator
 
 class TestMethodGraphGenerator(unittest.TestCase):
     dirpath = "./"
-
     def setUp(self):
-        self.methodgg = MethodGraphGenerator()
+        self.method = MethodGraphGenerator(self.dirpath)
 
     def tearDown(self):
         pass
 
     def test_init(self):
-        self.assertEqual(self.methodgg.dirpath, self.dirpath)
+        self.assertEqual(self.method.dirpath, self.dirpath)
+        pass
 
     def test_get_graph(self):
-        pass
+        self.assertTrue(self.method.get_graph())
+
 
 if __name__ == '__main__':
     unittest.main()
